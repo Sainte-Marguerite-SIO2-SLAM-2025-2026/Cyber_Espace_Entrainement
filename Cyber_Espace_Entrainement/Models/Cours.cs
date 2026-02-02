ae2085c9b8e4a0f438eabeb18175bc166731ea59
Clovis Jouanneau <clov72@gmail.com>
2026-02-02 02:17:28 +0100

    Commentaires viewModels Accueil + Clean relayCommand + Commencement Cours

---
+++ Cyber_Espace_Entrainement/Models/Cours.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Cyber_Espace_Entrainement.Models
+{
+    public class Cours
+    {
+        [Key]
+        [Column("ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column("Titre")]
+        public string Titre { get; set; } = string.Empty;
+
+        [Column("Definition")]
+        public string Definition { get; set; } = string.Empty;
+
+        [Column("Explication")]
+        public string Explication { get; set; } = string.Empty;
+
+        [Column("Exemple")]
+        public string Exemple { get; set; } = string.Empty;
+
+        [Column("Image1")]
+        public string Image1 { get; set; } = string.Empty;
+
+        [Column("Image2")]
+        public string Image2 { get; set; } = string.Empty;
+
+        [Column("Image3")]
+        public string Image3 { get; set; } = string.Empty;
+
+        [Column("Lien")]
+        public string Lien { get; set; } = string.Empty;
+
+        [Column("Theme")]
+        public string Theme { get; set; } = string.Empty;
+    }
+}
