88af5939eb57e9cf54608a73b6505e69c07a3b68
Clovis Jouanneau <jouanneauclovis@gmail.com>
2026-02-09 16:49:02 +0100

    Correction Injection

--- Cyber_Espace_Entrainement/Views/Activite/InjectionSQL/InjectionSqlMoyenView.xaml.cs
+++ Cyber_Espace_Entrainement/Views/Activite/InjectionSQL/InjectionSqlMoyenView.xaml.cs
@@ -10,14 +10,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Cyber_Espace_Entrainement.Views.Activite.InjectionSQL
+namespace Cyber_Espace_Entrainement.Views.Activite
 {
     /// <summary>
     /// Logique d'interaction pour InjectionSqlMoyenView.xaml
     /// </summary>
-    public partial class InjectionSqlMoyenView : Window
+    public partial class InjectionSQLMoyenView : Window
     {
-        public InjectionSqlMoyenView()
+        public InjectionSQLMoyenView()
         {
             InitializeComponent();
         }
