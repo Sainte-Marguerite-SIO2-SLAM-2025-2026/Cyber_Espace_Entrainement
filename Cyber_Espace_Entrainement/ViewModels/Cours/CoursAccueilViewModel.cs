69b271f1013211c1671b81fbf99fbdfdb6cd05d2
Clovis Jouanneau <jouanneauclovis@gmail.com>
2026-02-09 12:06:34 +0100

    Page d'accueil cours : add fonctionnel

--- Cyber_Espace_Entrainement/ViewModels/Cours/CoursAccueilViewModel.cs
+++ Cyber_Espace_Entrainement/ViewModels/Cours/CoursAccueilViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Cyber_Espace_Entrainement.Models;
 using Cyber_Espace_Entrainement.Services;
+using Cyber_Espace_Entrainement.Views.Cours;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -41,8 +43,27 @@ namespace Cyber_Espace_Entrainement.ViewModels.Cours
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Erreur de chargement : {ex.Message}");
+                MessageBoxService.ShowError($"Erreur de chargement : {ex.Message}");
             }
         }
+
+        #region Affichage Cours
+        
+        [RelayCommand]
+        public void OuvertureCours(Models.Cours unCours)
+        {
+            //Models.Cours unCours = _coursService.GetCoursByLibelle(libelleCours);
+
+            try {
+                CoursContenu CoursWindow = new CoursContenu(unCours);
+                CoursWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBoxService.ShowError($"Erreur d'ouverture : {ex.Message}");
+            }
+        }
+
+        #endregion
     }
 }
