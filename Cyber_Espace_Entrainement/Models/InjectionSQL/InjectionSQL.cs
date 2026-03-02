4017ca7e4fbda2083a7f119877bc30d7c8d81e00
Clovis Jouanneau <jouanneauclovis@gmail.com>
2026-03-02 15:30:14 +0100

    Envoie avant merge, mais erreur

---
+++ Cyber_Espace_Entrainement/Models/InjectionSQL/InjectionSQL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyber_Espace_Entrainement.Models.InjectionSQL
+{
+    public class InjectionSQL
+    {
+
+    }
+}
---
+++ Cyber_Espace_Entrainement/ViewModels/Activite/InjectionSQL/InjectionSQLViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyber_Espace_Entrainement.ViewModels.Activite
+{
+    public class InjectionSQLViewModel
+    {
+        public InjectionSQLViewModel()
+        {
+
+        }
+    }
+}
